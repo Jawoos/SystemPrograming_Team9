28b67ba0e10028a9a590bd64fa9dfc22ef72c3ed
Bread Park <jwpark@naver.com>
2020-11-25 20:03:59 +0900

    fix crash on dokill.c

--- dokill.c
+++ dokill.c
@@ -8,11 +8,7 @@ void do_kill(psinfo* ary, int size){
 		printf("There is nothing available process to kill\n");
 	for(int i = 0; i < size; i++){
 		printf("print check of pid(%d) : %d and my pid is %d\n", ary[i].pid, ary[i].checkTokill, getpid());
-<<<<<<< HEAD
-		if(ary[i].pid != getpid() && ary[i].checkTokill == 0){
-=======
-		if(ary[i].pid != getpid() && ary[i].checkTokill == 1 && ary[i].pid != getppid()){
->>>>>>> 2bf8185a7427daf9480676c6b9d07d97402029b0
+		if(ary[i].pid != getpid() && ary[i].checkTokill == 0 && ary[i].pid != getppid()){
 			printf("processor (%d) will be killed\n", ary[i].pid);
 			kill(ary[i].pid, SIGINT);
 		}
@@ -25,7 +21,7 @@ void do_must_kill(psinfo* ary, int size){
 		printf("There is nothing available process to kill\n");
 	for(int i = 0; i < size; i++){
 		printf("print check of pid(%d) : %d and my pid is %d ppid is %d\n", ary[i].pid, ary[i].checkTokill, getpid(), getppid());
-		if(ary[i].pid != getpid() && ary[i].checkTokill == 1 && ary[i].pid != getppid()){
+		if(ary[i].pid != getpid() && ary[i].checkTokill == 0 && ary[i].pid != getppid()){
 			printf("processor (%d) will be killed\n", ary[i].pid);
 		//	sleep(60);
 			kill(ary[i].pid, SIGKILL);
